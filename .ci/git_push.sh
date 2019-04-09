6bef1cac696388ca9b9f09954a7272f75b3abe26
taiyeba <staiyeba@gmail.com>
2019-04-09 14:54:28 +0200

    remote name fix

--- .ci/git_push.sh
+++ .ci/git_push.sh
@@ -4,6 +4,7 @@
 set_cred() {
   git config --global user.name "staiyeba"
   git config --global user.email "staiyeba@gmail.com"
+  git config --global user.password "${GH_TOKEN}"
 }
 
 git_commit() {
@@ -14,8 +15,8 @@ git_commit() {
 
 
 git_push() {
-  git remote add origin-ssh https://${GH_TOKEN}@github.com/travisci-cache.git
-  git push --set-upstream origin-ssh master
+#  git remote add origin https://github.com/staiyeba/travisci-cache.git
+  git push origin master
 }
 
 set_cred
