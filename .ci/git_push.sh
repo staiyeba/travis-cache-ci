551d799fc3e372d4988f19ac234fe4ff999b06d6
taiyeba <staiyeba@gmail.com>
2019-04-09 13:48:22 +0200

    add a git script

---
+++ .ci/git_push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+set_cred() {
+  - git config --global user.name "staiyeba"
+  - git config --global user.email "staiyeba@gmail.com"
+}
+
+git_commit() {
+  cd travisci-cache
+  git add *.tar.gz
+  git commit -m "Build number: $TRAVIS_BUILD_NUMBER"
+}
+
+
+git_push() {
+  git remote add origin-ssh https://${git_tok}@github.com/travisci-cache.git
+  git push --set-upstream origin-ssh master
+}
+
+set_cred
+git_commit
+git_push
